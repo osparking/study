31cc3935e38fbfc74c4e7ed3cc77ca6845475f59
osparking <jbpark03@gmail.com>
2023-01-20 07:31:08 +0900

    diary list select one line sql query

--- src/main/resources/sql/diary_crud.sql
+++ src/main/resources/sql/diary_crud.sql
@@ -3,3 +3,6 @@ insert into study.diary(title, content, study_date) values (
 	'3 가지 종류의 콤포넌트들 간에 정해진 임무가 무엇인지 학습하였다.',
 	'2023-01-13'
 );
+
+use study;
+select * from diary order by study_date desc;

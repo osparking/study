5b1a84b3e19aa9b3495a77a43fd284a282b52639
osparking <jbpark03@gmail.com>
2023-01-19 19:26:41 +0900

    mariaDB myself user ID/PW

---
+++ src/main/java/space/bumtiger/study/config/RootConfiguration.java
@@ -0,0 +1,27 @@
+package space.bumtiger.study.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class RootConfiguration {
+	@Bean
+	public NamedParameterJdbcTemplate getJdbcTemplate() {
+		var jdbcTemplate = new NamedParameterJdbcTemplate(getDataSource());
+		return jdbcTemplate;
+	}
+	
+	@Bean
+	public DataSource getDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName(org.mariadb.jdbc.Driver.class.getName());
+		ds.setUrl("jdbc:mariadb://localhost:3306/study");
+		ds.setUsername("myself");
+		ds.setPassword("1234");
+		return ds;		
+	}
+}

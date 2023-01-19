1d8105214ff301bb85c9738767c9dbc640330f80
osparking <jbpark03@gmail.com>
2023-01-20 07:29:37 +0900

    diary listing service method interface and it's implementation

--- src/main/java/space/bumtiger/study/service/DiaryService.java
+++ src/main/java/space/bumtiger/study/service/DiaryService.java
@@ -1,7 +1,11 @@
 package space.bumtiger.study.service;
 
+import java.util.List;
+
 import space.bumtiger.study.domain.Diary;
 
 public interface DiaryService {
 	public int save(Diary diary);
+
+	public List<Diary> listing();
 }
--- src/main/java/space/bumtiger/study/service/impl/DiaryServiceImpl.java
+++ src/main/java/space/bumtiger/study/service/impl/DiaryServiceImpl.java
@@ -1,5 +1,7 @@
 package space.bumtiger.study.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +10,19 @@ import space.bumtiger.study.repository.DiaryRepository;
 import space.bumtiger.study.service.DiaryService;
 
 @Service
-public class DiaryServiceImpl implements DiaryService{
+public class DiaryServiceImpl implements DiaryService {
 
 	@Autowired
 	public DiaryRepository diaryRepository;
+
 	@Override
 	public int save(Diary diary) {
 		return diaryRepository.save(diary);
+	}
+
+	@Override
+	public List<Diary> listing() {
+		return diaryRepository.listing();
 	}
 
 }

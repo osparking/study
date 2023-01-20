cfd855b04549ba16131fae19efe5aaf4b1342a04
osparking <jbpark03@gmail.com>
2023-01-20 10:00:53 +0900

    diary listing repository interface

--- src/main/java/space/bumtiger/study/repository/DiaryRepository.java
+++ src/main/java/space/bumtiger/study/repository/DiaryRepository.java
@@ -1,7 +1,11 @@
 package space.bumtiger.study.repository;
 
+import java.util.List;
+
 import space.bumtiger.study.domain.Diary;
 
 public interface DiaryRepository {
 	int save(Diary diary);
+
+	List<Diary> listing();
 }

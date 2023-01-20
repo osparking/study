a8b919916b4ee5b76b4e83a189cf882409682779
osparking <jbpark03@gmail.com>
2023-01-21 00:08:17 +0900

    private static final class for lambda

--- src/main/java/space/bumtiger/study/repository/impl/DiaryRepositoryImpl.java
+++ src/main/java/space/bumtiger/study/repository/impl/DiaryRepositoryImpl.java
@@ -1,10 +1,13 @@
 package space.bumtiger.study.repository.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -30,9 +33,15 @@ public class DiaryRepositoryImpl implements DiaryRepository {
 	@Override
 	public List<Diary> listing() {
 		String stmt = "select * from diary order by study_date desc";
-		return jdbcTemplate.query(stmt,
-				(rs, rN) -> new Diary(rs.getInt("sno"), rs.getString("title"),
-						rs.getString("content"), rs.getString("study_date"),
-						rs.getShort("duration_min")));
+		return jdbcTemplate.query(stmt, new DiaryMapper());
+	}
+
+	private static final class DiaryMapper implements RowMapper<Diary> {
+		@Override
+		public Diary mapRow(ResultSet rs, int rowNum) throws SQLException {
+			return new Diary(rs.getInt("sno"), rs.getString("title"),
+					rs.getString("content"), rs.getString("study_date"),
+					rs.getShort("duration_min"));
+		}
 	}
 }
